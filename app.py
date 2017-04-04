f295e7255fb4acd0480fbc74812f3c7fffca95ca
yawata159 <seijiyawata@gmail.com>
2017-04-04 01:09:43 -0400

    A

--- app.py
+++ app.py
@@ -9,8 +9,22 @@ def root():
 
 @app.route("/data/")
 def data():
-    ret = cars.get_cars(test=False)
-    return jsonify(ret)
+    makes = ['Mercury', 'Maserati', 'Chevrolet', 'Porsche', 'Lamborghini', 'Mercedes-Benz', 'Jaguar', 'Ford', 'Suzuki', 'Maybach', 'Dodge', 'Scion', 'Rolls-Royce', 'Cadillac', 'Honda', 'Hyundai', 'AMG', 'Volkswagen', 'Mazda', 'Jeep', 'Chrysler Group LLC', 'Infiniti', 'BMW', 'Kia', 'Mitsubishi', 'Land Rover', 'Grand Cherokee', 'MINI', 'Lincoln', 'Acura', 'Saab', 'GMC', 'Aston Martin', 'Nissan', 'Toyota', 'Volvo', 'BMW Motorrad', 'Ferrari', 'Chrysler', 'Mercedes', 'Mercedes-AMG', 'Bentley', 'Lotus', 'Subaru', 'Buick', 'Lexus', 'Audi']
+    formatted = {
+        2009: dict(),
+        2010: dict(),
+        2011: dict(),
+        2012: dict()
+    }
+
+    for year in xrange(2009, 2013):
+        for car in cars.get_cars_by_year(year, test=False):
+            make = car['Identification']['Make']
+            if make not in formatted[year]:
+                formatted[year][make] = list()
+            (formatted[year][make]).append(car)
+
+    return jsonify(formatted)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, host='0.0.0.0')
--- static/script.js
+++ static/script.js
@@ -1,12 +1,24 @@
 var car_data;
+$.getJSON("/data/", data_callback)
 
 function data_callback(r) {
     car_data = r;
-    main();
-};
-
-
-
-function main() {
-    console.log(car_data[0]);
 }
+
+$(document).ready( function() {
+    //console.log(car_data);
+    $('#years').slider({
+        min: 2009,
+        max: 2012,
+        create: function(e, ui) {
+            $("#custom-handle").text( $(this).slider("value") );
+        },
+        slide: function(e, ui) {
+            $("#custom-handle").text(ui.value);
+        },
+        stop: function(e, ui) {
+            console.log(ui.value);
+        }
+    });
+    $('#makes').selectmenu();
+});
