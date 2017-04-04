e59c7947eac2cc9d138e8cff6221766440281b01
yawata159 <seijiyawata@gmail.com>
2017-04-04 19:23:08 -0400

    Slider and dropdown

--- static/script.js
+++ static/script.js
@@ -1,14 +1,23 @@
-
-
+var car_data;
 $(document).ready( function() {
-    var car_data;
-    $.getJSON("/data/", data_callback)
-
-    function data_callback(r) {
+    $.getJSON("/data/", function(r) {
         car_data = r;
+    })
+
+    function update_dropdown(year) {
+        var dropdown = document.querySelector('#makes');
+        while (dropdown.firstElementChild) {
+            dropdown.remove(dropdown.firstElementChild);
+        }
+
+        for (make in car_data[year]) {
+            var new_node = document.createElement("option");
+            new_node.setAttribute("value", make);
+            new_node.innerHTML = make;
+            dropdown.appendChild(new_node);
+        }
     }
 
-    console.log(car_data);
     $('#years').slider({
         min: 2009,
         max: 2012,
@@ -19,8 +28,24 @@ $(document).ready( function() {
             $("#custom-handle").text(ui.value);
         },
         stop: function(e, ui) {
-            console.log(ui.value);
+            var year = ui.value;
+            update_dropdown(year)
+            display();
         }
     });
-    $('#makes').selectmenu();
+
+    $('#makes').on('change', function() {
+        display();
+    });
+
+    function display() {
+        var year = $('#years').slider("value");
+        var make = $('#makes').val();
+        console.log(make, year);
+        
+        //data for make and year
+        var car = car_data[year][make];
+        
+    }
+
 });
